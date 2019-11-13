67ff92e2fa5aee58804b9974adb14b97d5c79c77
Alimzhanov Amir <amir.a@tigla.ru>
2019-11-13 17:48:57 +0300

    Update Message,jsx: Create add message functions

--- src/components/dialogs/messages/Messages.jsx
+++ src/components/dialogs/messages/Messages.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import classes from './Messages.module.css';
 import Message from './message/Message';
-
+import { sendMessageCreator, updateNewMessageBodyCreator } from '../../../redux/reducers/dialogs-reducer'
 function Messages(props) {
 
-  let messagesList = props.messagesData.map( messageData => {
+  function onSendMessage() {
+    if ( props.state.newMessageBody.length > 0 ) {
+      props.dispatch( sendMessageCreator() )
+    }
+  }
+
+  function onNewMessageChange(e) {
+    props.dispatch( updateNewMessageBodyCreator(e.target.value) )
+  }
+
+  let newMessageBody = props.state.newMessageBody;
+
+  let messagesList = props.state.messagesData.map( (messageData, key) => {
     return (
-      <li className={classes['messages__message']}>
+      <li className={classes['messages__message']} key={key}>
         <Message name={ messageData.name } message={ messageData.message } />
       </li>
     )
@@ -18,10 +30,10 @@ function Messages(props) {
         { messagesList }
       </ul>
       <div className={classes['message-input']}>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <textarea value={ newMessageBody } onChange={ onNewMessageChange } name="" placeholder='Enter your message'></textarea>
       </div>
       <div className={classes['message-submit']}>
-        <button className="new-post__submit">Send</button>
+        <button className="new-post__submit" onClick={ onSendMessage }>Send</button>
       </div>
     </div>
   )
